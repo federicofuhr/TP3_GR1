646aa6fa08a8be2b5474c88585158100ac6fcc0a
martinmassimo <martin.massimo.91@gmail.com>
2020-10-04 17:13:16 -0300

    primeras incorporaciones en visualizacion

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -2,5 +2,27 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
     "use strict";
-    
+    var data = [
+    [
+        "Tiger Nixon",
+        "System Architect",
+        "Edinburgh",
+        "5421",
+        "2011/04/25",
+        "$3,120"
+    ],
+    [
+        "Garrett Winters",
+        "Director",
+        "Edinburgh",
+        "8422",
+        "2011/07/25",
+        "$5,300"
+    ]
+]
+    $(document).ready( function () {
+    $('#myTable').DataTable({
+    	data : data
+    });
+} );
 }
