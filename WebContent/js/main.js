0190f7758473321bfd8a0c6a467423a898e3f05b
martinmassimo <martin.massimo.91@gmail.com>
2020-10-04 18:51:41 -0300

    mejoras y paginas para navegar

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -2,27 +2,26 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
     "use strict";
-    var data = [
-    [
-        "Tiger Nixon",
-        "System Architect",
-        "Edinburgh",
-        "5421",
-        "2011/04/25",
-        "$3,120"
-    ],
-    [
-        "Garrett Winters",
-        "Director",
-        "Edinburgh",
-        "8422",
-        "2011/07/25",
-        "$5,300"
-    ]
-]
-    $(document).ready( function () {
-    $('#myTable').DataTable({
-    	data : data
-    });
-} );
+    let data = null;
+    getData();
+    async function getData() {
+        let r = await fetch("rest/estudiantes/listaEstudiantes");
+        let json =	 await r.json();
+        data = await json;
+        showData(data);
+    }
+    async function showData(data){
+		$('#myTable').DataTable( {
+     	data : data,
+	    columns: [
+	        { data: 'legajo' },
+	        { data: 'nombre' },
+	        { data: 'apellido' },
+	        { data: 'edad' },
+	        { data: 'genero' },
+	        { data: 'dni' },
+	        { data: 'ciudad_residencia' }
+		]
+		});
+    }
 }
